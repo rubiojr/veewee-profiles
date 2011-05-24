398addd59bb0e51a26936728f55441ea04fde7ad
Sergio Rubio <sergio@rubio.name>
2011-05-24 12:21:24 +0200

    * Use kernel.org mirrors

--- CentOS-5.6-x86_64-netboot/definition.rb
+++ CentOS-5.6-x86_64-netboot/definition.rb
@@ -3,7 +3,7 @@ Veewee::Session.declare({
   :disk_size => '10140', :disk_format => 'VDI', :hostiocache => 'off', :ioapic => 'on', :pae => 'on',
   :os_type_id => 'RedHat',
   :iso_file => "CentOS-5.6-x86_64-netinstall.iso",
-  :iso_src => "http://mirror.bcn.abiquo.com/centos/5.6/isos/x86_64/CentOS-5.6-x86_64-netinstall.iso",
+  :iso_src => "http://mirrors.kernel.org/centos/5.6/isos/x86_64/CentOS-5.6-x86_64-netinstall.iso",
   :iso_md5 => "a710105f7f9fe3516f08f6f8514ed2b0",
   :iso_download_timeout => 1000,
   :boot_wait => "10", :boot_cmd_sequence => [ 'linux text ks=http://%IP%:%PORT%/ks.cfg<Enter>' ],
