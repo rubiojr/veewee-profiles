24aadef1da5517037a1d82bf59db141c17fa6e0e
Sergio Rubio <rubiojr@frameos.org>
2011-08-16 17:03:01 +0200

    * new veewee profiles

---
+++ centos6-32-nochef-nopuppet/postinstall.sh
@@ -0,0 +1,29 @@
+#http://chrisadams.me.uk/2010/05/10/setting-up-a-centos-base-box-for-development-and-testing-with-vagrant/
+
+date > /etc/vagrant_box_build_time
+
+yum -y erase wireless-tools gtk2 libX11 hicolor-icon-theme avahi freetype bitstream-vera-fonts
+yum -y clean all
+
+# Installing vagrant keys
+mkdir /home/vagrant/.ssh
+chmod 700 /home/vagrant/.ssh
+cd /home/vagrant/.ssh
+wget --no-check-certificate 'http://github.com/mitchellh/vagrant/raw/master/keys/vagrant.pub' -O authorized_keys
+chown -R vagrant /home/vagrant/.ssh
+
+# Installing the virtualbox guest additions
+yum install -y kernel-devel kernel-headers gcc make gcc-c++
+VBOX_VERSION=$(cat /home/vagrant/.vbox_version)
+cd /tmp
+wget http://download.virtualbox.org/virtualbox/$VBOX_VERSION/VBoxGuestAdditions_$VBOX_VERSION.iso
+mount -o loop VBoxGuestAdditions_$VBOX_VERSION.iso /mnt
+sh /mnt/VBoxLinuxAdditions.run
+umount /mnt
+rm VBoxGuestAdditions_$VBOX_VERSION.iso
+yum remove -y kernel-devel kernel-headers gcc make gcc-c++
+yum -y clean all
+
+sed -i "s/^.*requiretty/#Defaults requiretty/" /etc/sudoers
+
+exit
